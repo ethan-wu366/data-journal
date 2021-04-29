05b232ed22e2ceeeb231eff1b6ff7b746ecb2922
Ethan Wu <ewu000@berkeley.edu>
2021-04-29 12:03:02 +0800

    not needed now

--- frontend/data-journal-v2/src/App.js
+++ frontend/data-journal-v2/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import Landing from './components/Landing';
+import About from './components/About';
+import Team from './components/Team';
+import Sponsors from './components/Sponsors';
+import Join from './components/Join';
+import Footer from './components/Footer';
+import Error from './components/404';
+
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/team">
+              <Team />
+            </Route>
+            <Route path="/sponsors">
+              <Sponsors />
+            </Route>
+            <Route path="/join">
+              <Join />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+            <Route default>
+              <Error />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
   );
 }
 
 export default App;
+
